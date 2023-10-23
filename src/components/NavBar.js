fd49a655036d7749e7dbcde6912db1b8207a63d1
小黄 <xiaohuang@xiaohuangdeMacBook-Air.local>
2023-10-24 01:08:00 +0900

    fixed bug

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -183,7 +183,7 @@ const NavBar = () => {
     
             <button
             onClick={() => setMode(mode === "light" ? "dark" : "light") }
-            className={`ml-3 flex items-center justify-center rounded-full p-1
+            className={`w-6 ml-3 flex items-center justify-center rounded-full p-1
             ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark"}
             `}
             >
